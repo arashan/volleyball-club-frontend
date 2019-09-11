e94a04842e3ff754380ca2064c9df836c9134f21
Lola <yakjkf@gmail.com>
2019-09-11 12:33:22 +0600

    HeaderSlider component added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
     <Header></Header>
+    <header-slider></header-slider>
   </div>
 </template>
 
 <script>
-  import './assets/style.sass';
-import Header from './components/Header.vue'
+  /*import './assets/style.sass';*/
+  import Header from './components/Header.vue'
+  import headerSlider from './components/HeaderSlider.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Header
+  export default {
+    name: 'app',
+    components: {
+      Header, headerSlider
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/components/HeaderSlider.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="header__slider">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 carousel-img" src="images/content/slider_1.jpg" alt="First slide">
+                    <div class="carousel-caption carousel-text d-none d-md-block">
+                        <h5 class="carousel-text__title">Играй с нами в <br> <span class="logo">Iskre</span></h5>
+                        <p class="carousel-text__descr">Волейбол моя жизнь, волейбол моя игра, с волейболом мы вместе, с волейболом навсегда</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 carousel-img" src="images/content/NatGeo06.jpg" alt="Second slide">
+                    <div class="carousel-caption carousel-text d-none d-md-block">
+                        <h5 class="carousel-text__title">Играй с нами в <br> <span class="logo">Iskre</span></h5>
+                        <p class="carousel-text__descr">Нет ничего лучше, чем прожить жизнь в стремлении стать совершеннее</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 carousel-img" src="images/content/NaturePatterns02.jpg" alt="Second slide">
+                    <div class="carousel-caption carousel-text d-none d-md-block">
+                        <h5 class="carousel-text__title">Играй с нами в <br> <span class="logo">Iskre</span></h5>
+                        <p class="carousel-text__descr">Каждая тренировка — это шаг вперед. Каждая пропущенная тренировка — это два шага назад</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 carousel-img" src="images/content/NatGeo17.jpg" alt="Third slide">
+                    <div class="carousel-caption carousel-text d-none d-md-block">
+                        <h5 class="carousel-text__title">Играй с нами в <br> <span class="logo">Iskre</span></h5>
+                        <p class="carousel-text__descr">Нет ничего лучше, чем прожить жизнь в стремлении стать совершеннее</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderSlider'
+    }
+</script>
