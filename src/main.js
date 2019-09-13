6d5cddef1f2e87f90dff5a6226eafca2e6ba921c
a_umut <askar.umut.kal@gmail.com>
2019-09-13 13:39:13 +0600

    Обновление стилей header, header-slider, довлен отображение меню в мобильном

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import Vue from 'vue';
-import App from './App.vue';
-
 import 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Vue from 'vue';
+import App from './App.vue';
 
 const moment = require('moment');
 require('moment/locale/ru');
