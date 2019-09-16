8c991eb9e037aa2454deaab67835e8d9a3107506
Lola <yakjkf@gmail.com>
2019-09-16 11:28:20 +0600

    gallery with pagination

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,34 +1,125 @@
 <template>
     <div class="page gallery">
         <h2 class="page__title">Галерея</h2>
+        <pagination
+                :current="currentPage"
+                :total="totalPhotos"
+                :perPage="perPage"
+                @page-changed="getPhotoToCurrentPage">
+        </pagination>
         <div class="page__inner">
-            <vue-gallery :images="images" :index="index" @close="index = null"></vue-gallery>
+
+            <vue-gallery :images="photos" :index="index" @close="index = null"></vue-gallery>
             <div
                 class="image"
-                v-for="(image, imageIndex) in images"
+                v-for="(image, imageIndex) in photos"
                 :key="imageIndex"
                 @click="index = imageIndex"
                 :style="{ backgroundImage: 'url(' + image + ')' }"
             ></div>
+
         </div>
     </div>
 </template>
 
 <script>
+    import pagination from './Pagination'
     export default {
         name: "Gallery",
+        components: {
+            pagination
+        },
         data: function () {
             return {
                 images: [
-                    'https://dummyimage.com/800/ffffff/000000',
-                    'https://dummyimage.com/1600/ffffff/000000',
-                    'https://dummyimage.com/1280/000000/ffffff',
-                    'https://dummyimage.com/400/000000/ffffff',
-                    'https://dummyimage.com/800/ffffff/000000',
-                    'https://dummyimage.com/1600/ffffff/000000'
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=1',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=2',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=3',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=4',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=5',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=6',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=7',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=8',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=9',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=10',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=11',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=12',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=13',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=14',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=15',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=16',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=17',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=18',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=19',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=20',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=21',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=22',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=23',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=24',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=25',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=26',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=27',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=28',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=29',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=31',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=32',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=33',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=34',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=35',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=36',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=37',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=38',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=39',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=40',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=41',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=42',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=43',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=44',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=45',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=46',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=47',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=48',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=49',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=50',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=51',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=52',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=53',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=54',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=55',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=56',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=57',
+                    'https://dummyimage.com/600x400/000/fff.jpg&text=58'
+
+
                 ],
-                index: null
+                index: null,
+                photos:[],
+                totalPhotos: 0,
+                perPage: 6,
+                currentPage: 1
             };
+        },
+        computed: {
+
+        },
+        methods: {
+            getPhotoToCurrentPage(page) {
+                let start = (page - 1) * this.perPage,
+                    end = start + this.perPage;
+
+                this.photos = this.images.slice(start, end);
+                this.currentPage = page;
+            },
+            nextPage() {
+                return this.currentPage++;
+            },
+            prevPage() {
+                return this.currentPage--;
+            }
+        },
+        mounted() {
+            this.totalPhotos = this.images.length;
+            this.getPhotoToCurrentPage(this.currentPage);
         }
     }
 </script>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="pagination">
+        <div class="pagination_btn pagination__left" v-if="hasPrev()">
+            <a href="#" @click="changePage(prevPage)">Предыдущая</a>
+        </div>
+        <div class="pagination__mid" >
+           <ul class="pagination__list">
+               <li v-if="hasFirst()"><a href="#" class="pagination__item" @click="changePage(1)">1</a></li>
+               <li v-if="hasFirst()" class="">...</li>
+               <li v-for="(page,i) in pages" :key="i" class="pagination__item">
+                   <a href="#" @click.prevent="changePage(page)">{{ page }}</a>
+               </li>
+               <li v-if="hasLast()" class="">...</li>
+               <li v-if="hasLast()" class="pagination__item"><a href="#" @click="changePage(totalPages)">{{totalPages}}</a></li>
+           </ul>
+        </div>
+        <div class="pagination_btn pagination__right" v-if="hasNext()">
+            <a href="#" @click="changePage(nextPage)">Следующая</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Pagination",
+        props: {
+            current: {
+                type: Number,
+                default: 1
+            },
+            total: {
+                type: Number,
+                default: 0
+            },
+            perPage: {
+                type: Number,
+                default: 6
+            },
+            pageRange: {
+                type: Number,
+                default: 2
+            }
+        },
+        computed : {
+            pages() {
+                let pages = [];
+
+                for(let i=this.rangeStart;  i<=this.rangeEnd; i++) {
+                    pages.push(i)
+                }
+
+                return pages;
+            },
+            rangeStart() {
+                let start = this.current - this.pageRange;
+                return start > 0 ? start : 1
+            },
+            rangeEnd() {
+                let end = this.current + this.pageRange;
+                return end < this.totalPages ? end : this.totalPages
+            },
+            nextPage() {
+                return this.current + 1;
+            },
+            prevPage() {
+                return this.current - 1;
+            },
+            totalPages() {
+                return Math.ceil(this.total / this.perPage)
+            }
+        },
+        methods: {
+            hasFirst() {
+                return this.rangeStart !== 1
+            },
+            hasLast() {
+              return this.rangeEnd < this.totalPages
+            },
+            hasPrev() {
+                return this.current > 1
+            },
+            hasNext() {
+                return this.current < this.totalPages
+            },
+            changePage(page) {
+                this.$emit('page-changed', page);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .pagination__left, .pagination__right {
+        margin-right: 10px;
+        padding: 5px;
+        border: 1px solid darkblue;
+        background: lightblue;
+    }
+    .pagination__list {
+        display: flex;
+    }
+    .pagination__item {
+        margin-left: 10px;
+    }
+    .pagination_btn {
+        margin: 0px 25px;
+    }
+</style>
