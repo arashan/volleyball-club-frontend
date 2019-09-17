6cace1c374b22b7c3cf0c036007b205802539649
a_umut <askar.umut.kal@gmail.com>
2019-09-17 15:52:28 +0600

    Обновление стилей формы регистрации

--- src/components/RegPage.vue
+++ src/components/RegPage.vue
@@ -2,7 +2,7 @@
     <div class="page registry">
         <h2 class="page__title">Регистрация</h2>
         <div class="page__inner">
-            <form>
+            <form class="registry-form">
                 <div class="form-group row">
                     <label for="name" class="col-sm-2 col-form-label">Имя</label>
                     <div class="col-sm-10">
@@ -22,9 +22,9 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="height_user" class="col-sm-2 col-form-label">Рост</label>
-                    <div class="col-sm-10">
-                        <input id="height_user" type="number" class="form-control" placeholder="Рост">
+                    <label for="birthDate" class="col-sm-2 col-form-label">Дата рождения</label>
+                    <div class="col-sm-2">
+                        <input id="birthDate" type="date" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
@@ -34,23 +34,33 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="pwd" class="col-sm-2 col-form-label">Пароль</label>
-                    <div class="col-sm-10">
-                        <input id="pwd" type="password" class="form-control" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="pwd_repeat" class="col-sm-2 col-form-label">Повторите пароль</label>
-                    <div class="col-sm-10">
-                        <input id="pwd_repeat" type="password" class="form-control" required>
-                    </div>
-                </div>
-                <div class="form-group row">
                     <label for="phone_num" class="col-sm-2 col-form-label">Телефон</label>
                     <div class="col-sm-10">
                         <input id="phone_num" type="text" class="form-control" placeholder="+(996) 700 999 999" required>
                     </div>
                 </div>
+                <div class="form-group row">
+                    <h4 class="title--acsent col-sm-12">Дополнительная информация</h4>
+                </div>
+                <div class="form-group form-inline">
+                    <label class="form-check-label col-form-label col-sm-1">
+                        <input class="form-check-input" type="radio" id="inlineCheckbox1" name="opt1" value="option1">Жен
+                    </label>
+                    <label class="form-check-label col-form-label col-sm-1">
+                        <input class="form-check-input" type="radio" id="inlineCheckbox2" name="opt1" value="option1">Муж
+                    </label>
+
+                    <label for="height_user" class="col-sm-1 col-form-label">Рост</label>
+                    <input id="height_user" type="number" class="form-control col-sm-2 mb-2 mr-sm-2 mb-sm-0" placeholder="Рост">
+
+                    <label for="chest_size" class="col-sm-1 col-form-label">ОГ</label>
+                    <input id="chest_size" type="number" class="form-control col-sm-2 mb-2 mr-sm-2 mb-sm-0" placeholder="Обхват груди">
+
+                    <label for="waist_size" class="col-sm-1 col-form-label">ОТ</label>
+                    <input id="waist_size" type="number" class="form-control col-sm-2 mb-2 mr-sm-2 mb-sm-0" placeholder="Обхват талии">
+
+                </div>
+
                 <div class="form-group row row--center">
                     <button class="btn btn-primary" type="submit">Готово</button>
                 </div>
