8bc12e99b8ea2850edd7d133d6629d0e538589ee
Lola <yakjkf@gmail.com>
2019-09-17 14:19:31 +0600

    pagination down

--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -11,8 +11,8 @@
                 <tbody>
                 <tr v-for="(rows, i) in timetable" :key="i">
                     <td v-for="(col, index) in rows" :key="index" @dblclick="editCell({'row': i, 'col': index}, $event)" class="disable-dbl-tap-zoom">
-                        <span class = "tableCell" v-show="isSpanVisible(i, index)" >
-                            {{col}}
+                        <span class = "tableCell" v-show="isSpanVisible(i, index)">
+                            {{ formatTime(col) }}
                         </span>
                     </td>
                 </tr>
@@ -21,7 +21,7 @@
             </table>
         </div>
 
-        <input ref="inputCell"
+<!--        <input ref="inputCell"
                type="text"
                class="form-control"
                v-model="currentCellValue"
@@ -29,40 +29,40 @@
                @keyup.13="saveCell"
                @keyup.esc="cancelSave"
                @blur="cancelSave"
-        />
+        />-->
 
         <!--<date-picker v-model="value12" lang="en" type="time" format="HH:mm:ss" placeholder="Select Time"></date-picker>-->
-        <!--<div class="timepicker-wrapper" ref="inputCell">
+        <div class="timepicker-wrapper" ref="inputCell">
             <datetime-picker
+                    :time-picker-options="timePickerOptions"
                     v-model="currentCellValue"
                     lang="ru"
                     type="time"
                     format="HH:mm"
                     placeholder="Выберите Время"
                     v-show="currentCell"
-                    @change="blurFunct"
+                    @change="saveCell"
             />
-        </div>-->
+        </div>
 
     </div>
 </template>
 
 <script>
+    const moment = require('moment');
     export default {
         name: "Timetable",
         data: function () {
             return {
                 weekDays: ["Пн","Вт","Ср","Чт","Пт","Сб","Вс"],
-                timetable: [
-                    ['X', '', '', '', '', '', '' ],
-                    ['', '', '', 'X', '', '', '' ],
-                    ['', 'X', '', '', '', '', '' ],
-                    ['', '', '', '', '', 'X', '' ],
-                    ['X', '', '', '', '', '', '' ]
-
-                ],
+                timetable: null,
                 currentCell: null,
-                currentCellValue: null
+                currentCellValue: null,
+                timePickerOptions:{
+                    start: '07:00',
+                    step: '00:15',
+                    end: '21:30'
+                }
             }
         },
         watch: {
@@ -72,12 +72,26 @@
             }*/
         },
         methods : {
+            prepareData(data) {
+                /*return data.map(function(row) {
+                    return row.map(function(col) {
+                        let colVal = col !== '' ? new Date(col).getTime() : new Date('2019-01-01T01:00').getTime();
+                        return colVal;
+                    })
+                });*/
+                return data;
+            },
+            loadData() {
+                const data = require("../assets/data/timetable.json");
+                this.timetable = this.prepareData(data);
+            },
             isSpanVisible(row, col) {
                 if (this.currentCell) {
                     if (this.currentCell.row === row && this.currentCell.col === col) {
                         return false;
                     }
                 }
+
                 return true;
             },
             editCell(cell, event) {
@@ -89,7 +103,7 @@
 
                 setTimeout(() => {
                     this.$refs.inputCell.focus();
-                }, 500);
+                }, 100);
             },
             saveCell() {
                 this.timetable[this.currentCell.row][this.currentCell.col] = this.currentCellValue;
@@ -97,7 +111,18 @@
             },
             cancelSave() {
                 this.currentCell = null;
+            },
+            formatTime(colVal) {
+                let res = '';
+                if (Date.parse(colVal)) {
+                    let ddate = moment(colVal);//new Date(colVal).getTime();
+                    res = ddate.format('hh:mm')
+                }
+                return res;
             }
+        },
+        mounted() {
+            this.loadData();
         }
     }
 </script>
