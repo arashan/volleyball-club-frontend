1cc1669111917e5c3ca4251ea909c271abc08e7d
Lola <yakjkf@gmail.com>
2019-09-20 12:03:26 +0600

    pagination for News

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,21 +2,32 @@
     <div class="page">
         <h2 class="page__title">Новости</h2>
         <div class="page__inner">
-            <news-item v-for="article in articleList" :key="article.id" :article="article"></news-item>
+            <news-item v-for="article in articles" :key="article.id" :article="article"></news-item>
         </div>
+        <pagination
+                :current="currentPage"
+                :total="totalNews"
+                :perPage="perPage"
+                @page-changed="getArticlesToCurrentPage">
+        </pagination>
     </div>
 </template>
 
 <script>
     import newsItem from "./NewsItem"
+    import pagination from './Pagination'
     export default {
         name: "News",
         components: {
-            newsItem
+            newsItem, pagination
         },
         data: function () {
             return {
-                articleList : []
+                articleList : [],
+                articles:[],
+                totalNews: 0,
+                perPage: 3,
+                currentPage: 1
             }
         },
         methods: {
@@ -31,10 +42,21 @@
 
                     return item;
                 });
+            },
+            getArticlesToCurrentPage(page) {
+                let start = (page - 1) * this.perPage,
+                    end = start + this.perPage;
+
+                this.articles = this.articleList.slice(start, end);
+                this.currentPage = page;
             }
         },
         created() {
             this.loadData();
+        },
+        mounted() {
+            this.totalNews = this.articleList.length;
+            this.getArticlesToCurrentPage(this.currentPage);
         }
     }
 </script>
